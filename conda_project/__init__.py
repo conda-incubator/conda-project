40be6e50803ba4f13ad1b5a4036c2a06f8cccd37
Matt Kramer <mkramer@anaconda.com>
2022-04-11 11:38:58 -0500

    Exclude versioneer file from mypy

--- conda_project/__init__.py
+++ conda_project/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2022 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-from ._version import get_versions
+from ._version import get_versions  # type: ignore
 
 __version__ = get_versions()["version"]
 del get_versions
