01bc7285278568cd23b0e322ac8af7b7ce0d9e70
mattkram <mkramer@anaconda.com>
2023-04-05 10:54:03 -0500

    Write the version to _version.py so it is included into the source tarball

--- src/conda_project/__init__.py
+++ src/conda_project/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2022 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-import importlib.metadata
 
-__version__ = importlib.metadata.version("conda-project")
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
 
 # flake8: noqa
 from .exceptions import CondaProjectError
