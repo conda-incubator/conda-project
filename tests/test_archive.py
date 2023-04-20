b7b801a50fcb446b72d17c2b7c10260c167a136b
Albert DeFusco <adefusco@anaconda.com>
2023-04-20 15:32:23 -0500

    sorting is different on windows

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -22,21 +22,22 @@ PROJECT_CONTENTS = [
 ]
 
 
-def test_extract_top_level_dir(tmp_path, monkeypatch):
+@pytest.fixture(scope="function", autouse=True)
+def chdir(tmp_path, monkeypatch):
     monkeypatch.chdir(tmp_path)
 
+
+def test_extract_top_level_dir():
     archive = ASSETS_DIR / "top-level-dir.tar.gz"
     project = CondaProject.from_archive(fn=archive)
 
     assert project.directory.name == "my-project"
 
     dir_contents = sorted(list(project.directory.glob("**/*")))
-    assert dir_contents == [project.directory / p for p in PROJECT_CONTENTS]
+    assert dir_contents == sorted([project.directory / p for p in PROJECT_CONTENTS])
 
 
-def test_extract_top_level_dir_rename(tmp_path, monkeypatch):
-    monkeypatch.chdir(tmp_path)
-
+def test_extract_top_level_dir_rename():
     archive = ASSETS_DIR / "top-level-dir.tar.gz"
     project = CondaProject.from_archive(
         fn=archive, output_directory="extracted-project"
@@ -45,28 +46,24 @@ def test_extract_top_level_dir_rename(tmp_path, monkeypatch):
     assert project.directory.name == "extracted-project"
 
     dir_contents = sorted(list(project.directory.glob("**/*")))
-    assert dir_contents == [
-        ("extracted-project" / p).resolve() for p in PROJECT_CONTENTS
-    ]
+    assert dir_contents == sorted(
+        [("extracted-project" / p).resolve() for p in PROJECT_CONTENTS]
+    )
 
 
-def test_extract_no_top_level_dir(tmp_path, monkeypatch):
-    monkeypatch.chdir(tmp_path)
-
+def test_extract_no_top_level_dir():
     archive = ASSETS_DIR / "no-top-level-dir.tar.gz"
     project = CondaProject.from_archive(fn=archive)
 
     assert project.directory.name == "no-top-level-dir"
 
     dir_contents = sorted(list(project.directory.glob("**/*")))
-    assert dir_contents == [
-        ("no-top-level-dir" / p).resolve() for p in PROJECT_CONTENTS
-    ]
+    assert dir_contents == sorted(
+        [("no-top-level-dir" / p).resolve() for p in PROJECT_CONTENTS]
+    )
 
 
-def test_extract_no_top_level_dir_rename(tmp_path, monkeypatch):
-    monkeypatch.chdir(tmp_path)
-
+def test_extract_no_top_level_dir_rename():
     archive = ASSETS_DIR / "no-top-level-dir.tar.gz"
     project = CondaProject.from_archive(
         fn=archive, output_directory="extracted-project"
@@ -75,28 +72,24 @@ def test_extract_no_top_level_dir_rename(tmp_path, monkeypatch):
     assert project.directory.name == "extracted-project"
 
     dir_contents = sorted(list(project.directory.glob("**/*")))
-    assert dir_contents == [
-        ("extracted-project" / p).resolve() for p in PROJECT_CONTENTS
-    ]
+    assert dir_contents == sorted(
+        [("extracted-project" / p).resolve() for p in PROJECT_CONTENTS]
+    )
 
 
-def test_extract_unnamed_top_level_dir(tmp_path, monkeypatch):
-    monkeypatch.chdir(tmp_path)
-
+def test_extract_unnamed_top_level_dir():
     archive = ASSETS_DIR / "unnamed-top-level-dir.tar.gz"
     project = CondaProject.from_archive(fn=archive)
 
     assert project.directory.name == "unnamed-top-level-dir"
 
     dir_contents = sorted(list(project.directory.glob("**/*")))
-    assert dir_contents == [
-        ("unnamed-top-level-dir" / p).resolve() for p in PROJECT_CONTENTS
-    ]
+    assert dir_contents == sorted(
+        [("unnamed-top-level-dir" / p).resolve() for p in PROJECT_CONTENTS]
+    )
 
 
-def test_extract_unnamed_top_level_dir_rename(tmp_path, monkeypatch):
-    monkeypatch.chdir(tmp_path)
-
+def test_extract_unnamed_top_level_dir_rename():
     archive = ASSETS_DIR / "unnamed-top-level-dir.tar.gz"
     project = CondaProject.from_archive(
         fn=archive, output_directory="extracted-project"
@@ -105,14 +98,12 @@ def test_extract_unnamed_top_level_dir_rename(tmp_path, monkeypatch):
     assert project.directory.name == "extracted-project"
 
     dir_contents = sorted(list(project.directory.glob("**/*")))
-    assert dir_contents == [
-        ("extracted-project" / p).resolve() for p in PROJECT_CONTENTS
-    ]
+    assert dir_contents == sorted(
+        [("extracted-project" / p).resolve() for p in PROJECT_CONTENTS]
+    )
 
 
-def test_extract_fails_relative_paths(tmp_path, monkeypatch):
-    monkeypatch.chdir(tmp_path)
-
+def test_extract_fails_relative_paths():
     archive = ASSETS_DIR / "relative-paths.tar.gz"
 
     with pytest.raises(CondaProjectError):
@@ -122,13 +113,13 @@ def test_extract_fails_relative_paths(tmp_path, monkeypatch):
 def test_archive_storage_options(mocker):
     from conda_project.project import fsspec
 
-    open_files = mocker.spy(fsspec, "open_files")
+    mocked_open_files = mocker.spy(fsspec, "open_files")
 
     fn = ASSETS_DIR / "top-level-dir.tar.gz"
     _ = CondaProject.from_archive(
         f"file:///{fn}", storage_options={"key1": "valueA", "key2": "valueB"}
     )
 
-    assert open_files.call_args_list[0].kwargs == {
+    assert mocked_open_files.call_args_list[0].kwargs == {
         "file": {"key1": "valueA", "key2": "valueB"}
     }
