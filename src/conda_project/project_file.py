ab748381d5a0c2424a410e625dc19bee6a2d82a0
Albert DeFusco <adefusco@anaconda.com>
2023-07-05 10:58:55 -0500

    and/remove operations on EnvironmentYaml model
    Added a model for "channels:" to provide
    an ordered unique list.
    
    The add behavior matches the "conda install" experience:
    if you add a package that already exists with a different matchspec,
    that package gets replaced. See tests
    for more details.

--- src/conda_project/project_file.py
+++ src/conda_project/project_file.py
@@ -3,9 +3,11 @@
 from __future__ import annotations
 
 import json
+from collections.abc import Iterable
 from pathlib import Path
-from typing import Dict, List, Optional, OrderedDict, TextIO, Union
+from typing import Any, Dict, List, Optional, OrderedDict, TextIO, Union
 
+from conda_lock._vendor.conda.models.match_spec import MatchSpec
 from pydantic import BaseModel, ValidationError, validator
 from ruamel.yaml import YAML
 
@@ -66,9 +68,41 @@ class CondaProjectYaml(BaseYaml):
     commands: OrderedDict[str, Union[Command, str]] = OrderedDict()
 
 
+class UniqueOrderedList(list):
+    def __init__(self, iterable: Iterable):
+        uniques = []
+        for item in iterable:
+            if item not in uniques:
+                uniques.append(item)
+
+        super().__init__(uniques)
+
+    def _remove_duplicates(self, other: Any):
+        for item in self:
+            if item in other:
+                other.remove(item)
+
+    def append(self, __object: Any) -> None:
+        if __object not in self:
+            return super().append(__object)
+
+    def extend(self, __iterable: Iterable) -> None:
+        self._remove_duplicates(__iterable)
+        return super().extend(__iterable)
+
+
+def replace_duplicate_matchspec_name(dependencies, to_update):
+    names = [MatchSpec(d).name for d in dependencies if not isinstance(d, dict)]
+    for dep in to_update:
+        m = MatchSpec(dep)
+        if m.name in names:
+            idx = names.index(m.name)
+            dependencies[idx] = dep
+
+
 class EnvironmentYaml(BaseYaml):
     name: Optional[str] = None
-    channels: Optional[List[str]] = None
+    channels: Optional[Union[UniqueOrderedList, List[str]]] = None
     dependencies: List[Union[str, Dict[str, List[str]]]] = []
     variables: Optional[Dict[str, str]] = None
     prefix: Optional[Path] = None
@@ -83,3 +117,46 @@ class EnvironmentYaml(BaseYaml):
                         f'The dependencies key contains an invalid map {item}. Only "pip:" is allowed.'
                     )
         return v
+
+    @validator("channels")
+    def convert_channels_list(cls, v):
+        if not isinstance(v, UniqueOrderedList):
+            return UniqueOrderedList(v)
+
+    @property
+    def matchspecs(self):
+        return [
+            MatchSpec(dep) for dep in self.dependencies if not isinstance(dep, dict)
+        ]
+
+    def add_dependencies(
+        self,
+        dependencies: List[str],
+        channels: Optional[Union[UniqueOrderedList, List[str]]] = None,
+    ) -> None:
+        current_names = [dep.name for dep in self.matchspecs]
+
+        to_add = []
+        for dep in dependencies:
+            name = MatchSpec(dep).name
+            if name in current_names:
+                self.dependencies[current_names.index(name)] = dep
+            else:
+                to_add.append(dep)
+
+        self.dependencies.extend(to_add)
+
+        if channels:
+            if self.channels:
+                self.channels.extend(channels)
+            else:
+                self.channels = UniqueOrderedList(channels)
+
+    def remove_dependencies(self, dependencies: List[str]) -> None:
+        current_names = [dep.name for dep in self.matchspecs]
+
+        for dep in dependencies:
+            name = MatchSpec(dep).name
+            if name in current_names:
+                self.dependencies.pop(current_names.index(name))
+                current_names.remove(name)
