b6d38f30821bb995cd5f0d2f2d93e7d604b27dac
Albert DeFusco <adefusco@anaconda.com>
2024-04-02 22:17:28 -0500

    add no cover

--- src/conda_project/project_file.py
+++ src/conda_project/project_file.py
@@ -11,13 +11,13 @@ from conda_lock._vendor.conda.models.match_spec import MatchSpec
 from pkg_resources import Requirement
 from ruamel.yaml import YAML
 
-try:
+try:  # pragma: no cover
     # Version 2 provides a v1 API
-    from pydantic.v1 import BaseModel, ValidationError, validator
-except ImportError:
-    from pydantic import BaseModel  # type: ignore
-    from pydantic import ValidationError  # type: ignore
-    from pydantic import validator  # type: ignore
+    from pydantic.v1 import BaseModel, ValidationError, validator  # pragma: no cover
+except ImportError:  # pragma: no cover
+    from pydantic import BaseModel  # type: ignore; #pragma: no cover
+    from pydantic import ValidationError  # type: ignore; #pragma: no cover
+    from pydantic import validator  # type: ignore; #pragma: no cover
 
 from .exceptions import CondaProjectError
 
